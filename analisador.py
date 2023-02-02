7c791793245d9d414d0e30e3046a4589b2959353
cardosojse <caardoso4@outlook.com>
2023-02-02 11:40:12 -0300

    Análise de grupo
    Neste programa, o usuário deverá escolher quantas pessoas ele irá analisar. O programa obterá a media de idade entre as pessoas, como também o homem mais velho e o seu nome e, por último, quantas mulheres possuem menos de 20 anos.

--- analisador.py
+++ analisador.py
@@ -10,7 +10,8 @@ for c in range(1, n+1):
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
     sexo = str(input('SEXO [M/F]: ')).upper()
-    sI += idade
+    sI = sI + idade
+    media = sI / 4
     if sexo == 'M':
         if c == 1:
             mI = idade
@@ -20,8 +21,7 @@ for c in range(1, n+1):
             nM = nome
     else:
         if idade < 20:
-            sM += 1
-media = sI / n
+            sM = sM + 1
 print(f'A média de idade do grupo é {media:.1f} anos')
 print(f'O homem mais velho do grupo tem {mI} anos e se chama {nM}.')
 print(f'Ao todo são {sM} mulheres com menos de 20 anos.')
