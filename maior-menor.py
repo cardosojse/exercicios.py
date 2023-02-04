26033dff1013f9415879e7c088bf72487133a857
cardosojse <caardoso4@outlook.com>
2023-02-04 19:10:16 -0300

    Maior e menor
    Programa simples que vai te pedir pra digitar um número e mostrar diversos valores: Soma total; Média; Maior e menor;

---
+++ maior-menor.py
@@ -0,0 +1,21 @@
+total = count = average = more = less = 0
+awnser = 'Yes'
+while awnser in 'YESyes':
+    number = int(input('Type a number: '))
+    total += number
+    count += 1
+    awnser = str(input('Wanna keep going? [YES or NO]: ')).upper().strip()[0]
+    if count == 1:
+        more = less = number
+    else:
+        if number > more:
+            more = number
+        if number < less:
+            less = number
+average = total / count
+print('It´s over')
+print(f'''Total sum = {total}
+Average = {average:.1f}
+Biggest = {more} 
+Smaller = {less}''')
+
