7806fc1ab8d9d4f72c9200c5bb7acd5bb6bfbc0b
cardosojse <caardoso4@outlook.com>
2023-02-07 10:59:43 -0300

    Update analisador.py

--- analisador.py
+++ analisador.py
@@ -1,31 +1,28 @@
 media = 0
-sI = 0  # sI é a soma das idades.
-mI = 0  # mI é a maior idade entre os homens.
-sM = 0  # sM é a soma de mulheres com menos de 20 anos.
-nM = 0  # nM é o nome do homem com maior idade.
+sI = mI = sM = nM = 0
+# sI é a soma das idades.
+# mI é a maior idade entre os homens.
+# sM é a soma de mulheres com menos de 20 anos.
+# nM é o nome do homem com maior idade.
 
 n = int(input('Quantas pessoas você irá entrevistar? '))
 
 for c in range(1, n+1):
 
     print(f'=-=-= {c}º PESSOA =-=-=')
-
     nome = str(input('Nome: '))
     idade = int(input('Idade: '))
     sexo = str(input('SEXO [M/F]: ')).upper()
-
     sI = sI + idade
     media = sI / n
 
     if sexo == 'M':
-
         if c == 1:
             mI = idade
             nM = nome
         elif idade > mI:
             mI = idade
             nM = nome
-
     else:
         if idade < 20:
             sM = sM + 1
