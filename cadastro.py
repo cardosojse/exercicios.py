68d45a4d009b2e30719d71e5293cdd57daf1868e
cardosojse <caardoso4@outlook.com>
2023-02-11 18:56:28 -0300

    Update cadastro.py

--- cadastro.py
+++ cadastro.py
@@ -1,7 +1,6 @@
 data = []
 register = []
 count = maior = menor = 0
-
 while True:
     count += 1
     print(f'{count}º PESSOA')
@@ -16,20 +15,16 @@ while True:
             menor = register[1]
     data.append(register[:])
     register.clear()
-
     awnser = ' '
     while awnser not in 'SN':
         awnser = str(input('Deseja continuar?[S/N]: ')).upper()
     if awnser == 'N':
         break
-
 print(f'Você cadastrou {len(data)} pessoas.')
-
 print(f'O maior peso foi {maior}kg. Peso de:', end=' ')
 for p in data:
     if p[1] == maior:
         print(f'{p[0]}')
-
 print(f'O menor peso foi {menor}kg. Peso de:', end=' ')
 for p in data:
     if p[1] == menor:
