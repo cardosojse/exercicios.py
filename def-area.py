9a25c13715a44eabce2ad8bfba6858127d97fddc
cardosojse <caardoso4@outlook.com>
2023-02-14 15:51:27 -0300

    Create def-area.py
    Meu primeiro programa utilizando funções.

---
+++ def-area.py
@@ -0,0 +1,9 @@
+def area():
+    a = b * h
+    print(f'A área do terreno {b}x{h} corresponde a {a}m²')
+
+
+# Programa principal
+b = float(input('Comprimento: '))
+h = float(input('Largura: '))
+area()
